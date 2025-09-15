0e8eabd9111b1375ed8a13777c70bd2973633155
Gustavo Carvalho <ogustavoress@gmail.com>
2025-09-15 02:03:24 -0300

    feat: Ajustando o build da SelectScreen para fazer a leitura dos times

--- lib/screens/select/select_screen.dart
+++ lib/screens/select/select_screen.dart
@@ -1,10 +1,32 @@
+import 'package:favorite_sport/data/sports_repository.dart';
+import 'package:favorite_sport/data/user_settings_repository.dart';
+import 'package:favorite_sport/model/Sport.dart';
 import 'package:flutter/material.dart';
 
-class SelectScreen extends StatelessWidget {
+class SelectScreen extends StatefulWidget {
   const SelectScreen({super.key});
 
   @override
+  State<SelectScreen> createState() => _SelectScreenState();
+}
+
+class _SelectScreenState extends State<SelectScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final userSettingsRepository = UserSettingsRepository();
+    final sportsRepository = SportsRepository();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Escolha seu esporte'),
+      ),
+      body: FutureBuilder<List<Sport>>(
+        future: sportsRepository.load(),
+        builder: (context, snapshot) {
+
+        },
+      ),
+    );
   }
 }
