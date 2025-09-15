270de30df9068dbfd6dcc5f823c6b00351f189e1
Gustavo Carvalho <ogustavoress@gmail.com>
2025-09-14 23:13:02 -0300

    feat: Adicionando o método que recupera o esporte selecionado pelo usuário

--- lib/data/user_settings_repository.dart
+++ lib/data/user_settings_repository.dart
@@ -1,3 +1,24 @@
+import 'dart:convert';
+
+import 'package:favorite_sport/model/Sport.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class UserSettingsRepository {
   static const _key = 'favorite_team';
+
+  Future<Sport?> getSport() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonStr = prefs.getString(_key);
+
+    if(jsonStr == null) {
+      return null;
+    }
+    try {
+      final map = jsonDecode(jsonStr) as Map<String, dynamic>;
+      return Sport.fromJson(map);
+    }
+    catch(_) {
+      return null;
+    }
+  }
 }
