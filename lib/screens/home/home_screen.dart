678a29f93e847712b0fa2b21c5c7a0a265e1df84
Gustavo Carvalho <ogustavoress@gmail.com>
2025-09-14 23:46:50 -0300

    feat: Criando os objetos UserSettingsRepository

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,19 @@
+import 'package:favorite_sport/data/user_settings_repository.dart';
+import 'package:favorite_sport/model/Sport.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  final userSettingsRepository = UserSettingsRepository();
+
+  Future<Sport?>?_future;
 
   @override
   Widget build(BuildContext context) {
