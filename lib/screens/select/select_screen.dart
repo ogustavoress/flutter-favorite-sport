929b9615d0bde806b5a6357a582af3686cc3cc95
Gustavo Carvalho <ogustavoress@gmail.com>
2025-09-14 23:21:36 -0300

    Criando as telas, Home e Select

---
+++ lib/screens/select/select_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelectScreen extends StatelessWidget {
+  const SelectScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
