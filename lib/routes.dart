01559f4ad9597cccc6e159bd9fc38939ddab0145
Gustavo Carvalho <ogustavoress@gmail.com>
2025-09-14 23:28:04 -0300

    feat: Adicionando interpretação das rotas

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,26 @@
+import 'package:favorite_sport/screens/home/home_screen.dart';
+import 'package:favorite_sport/screens/select/select_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class Routes {
   static const String home = '/';
   static const String select = '/select';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case home:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
+      case select:
+        return MaterialPageRoute(builder: (_) => SelectScreen());
+      default:
+        return MaterialPageRoute(builder: (_) =>
+          Scaffold(
+            body: Center(
+              child: Text('Rota não encontrada!'),
+            )
+          )
+        );
+    }
+  }
 }
