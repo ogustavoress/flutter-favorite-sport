9bbe149ab6de4764751eb16f48c8a8c5835db324
Gustavo Carvalho <ogustavoress@gmail.com>
2025-09-22 09:54:04 -0300

    feat: sobrescrevendo o método initState

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:favorite_sport/data/user_settings_repository.dart';
+import 'package:favorite_sport/routes.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -9,9 +11,21 @@ class SplashScreen extends StatefulWidget {
   }
 
 class _SplashScreenState extends State<SplashScreen> {
-
+  final userSettingsRepository = UserSettingsRepository();
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  void initState() {
+    super.initState();
+    _navigate();
+  }
+
+  Future<void> _navigate() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final showIntro = await userSettingsRepository.getShowIntro();
+      if (!mounted) return;
+      if (showIntro) {
+        Navigator.pushReplacementNamed(context, Routes.intro);
+      } else {
+        Navigator.pushReplacementNamed(context, Routes.home);
+      }
   }
 }
