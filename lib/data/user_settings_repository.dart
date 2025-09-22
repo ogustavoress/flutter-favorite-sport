6f7680ceef50994edc8aaafaa22f0f14413f5369
Gustavo Carvalho <ogustavoress@gmail.com>
2025-09-22 09:44:50 -0300

    feat: adicionando métodos para IntroScreen

--- lib/data/user_settings_repository.dart
+++ lib/data/user_settings_repository.dart
@@ -4,7 +4,7 @@ import 'package:favorite_sport/model/Sport.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserSettingsRepository {
-  static const _key = 'favorite_team';
+  static const _key = 'favorite_sport';
 
   Future<Sport?> getSport() async {
     final prefs = await SharedPreferences.getInstance();
@@ -31,4 +31,16 @@ class UserSettingsRepository {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_key);
   }
+
+  static const _keyShowIntro = 'show_intro';
+  Future<bool> getShowIntro() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_keyShowIntro) ?? true;
+  }
+
+  Future<void> setShowIntro(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_key, value);
+  }
+
 }
